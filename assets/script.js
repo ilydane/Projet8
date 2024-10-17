c1a0b85c9acd8a4fcd7205faf550ca5782ae68f4
ilydane <leoo75018@gmail.com>
2024-10-17 22:26:23 +0200

    mise en forme du carousel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,38 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+
+
+
+
+// JavaScript pour le carrousel
+const images = document.querySelectorAll('.carousel-image');
+const leftArrow = document.querySelector('.arrow_left');
+const rightArrow = document.querySelector('.arrow_right');
+let currentIndex = 0;
+
+// Fonction pour afficher une image spécifique
+function showImage(index) {
+   images.forEach((img, i) => {
+      img.classList.remove('active'); // Retire la classe active de toutes les images
+      if (i === index) {
+         img.classList.add('active'); // Ajoute la classe active uniquement à l'image correspondante
+      }
+   });
+}
+
+// Affiche la première image au chargement
+showImage(currentIndex);
+
+// Gestion des flèches
+leftArrow.addEventListener('click', () => {
+   currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+   showImage(currentIndex);
+});
+
+rightArrow.addEventListener('click', () => {
+   currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+   showImage(currentIndex);
+});
